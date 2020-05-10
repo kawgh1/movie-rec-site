e7e717e79a43415021fd116d7c5b777195d951cd
thrwwy216 <thrwwy216@tutanota.com>
2020-05-10 02:26:15 -0500

    omg

--- movieRecFlask/catalog/recommender1.py
+++ movieRecFlask/catalog/recommender1.py
@@ -70,7 +70,7 @@ movies_with_tags['tag'] = movies_with_tags['tag'].fillna(' ')
 movies_with_tags['genres'] = movies_with_tags['genres'].fillna(' ')
 
 movies_with_tags['userId'] = movies_with_tags['userId'].fillna(0.0)
-movies_with_tags['userId'] = movies_with_tags['userId'].astype(int)
+movies_with_tags['userId'] = movies_with_tags['userId'].apply(np.int32)
 # make sure userId column is ints and not floats for the database
 
 
