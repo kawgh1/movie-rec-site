d1e3653cdea2c3dfc614653deefc2f0a35ce9286
thrwwy216 <thrwwy216@tutanota.com>
2020-05-09 20:06:57 -0500

    ok

--- run.py
+++ run.py
@@ -20,55 +20,64 @@ from flask_sqlalchemy import SQLAlchemy
 # - WEB HOST SETTINGS - check settings in dashboard1.py as well
 ################################################################
 
-flask_app = create_app('prod')
-
-with flask_app.app_context():
-
-    recommender_db.create_all()
-
-    # If any of the User, RecsClicks or Logins tables are empty in Postgres,
-    # Create an entry for them to initilialize
-    # This is because other functions (Dashboard, etc.) require data in these tables to be valid
-    try:
-        if not User.query.filter_by(user_name='harry').first():
-            User.create_user(user='harry', email='harry@potters.com', password='secret')
-
-        if not RecsClicks.query.filter_by(user_id=0).first():
-            RecsClicks.record_getrecs(user_id=0, movie_id=0, movie='None')
-
-        if not Logins.query.filter_by(userid=0).first():
-            Logins.record_login(userid=0)
-
-        # Dash app functions require these tables below before it can be initiliazed
-        dash_app = create_dashboard(flask_app)
-
-    except exc.IntegrityError:
-
-        flask_app.run(debug=False)
+# flask_app = create_app('prod')
+#
+# with flask_app.app_context():
+#
+#     recommender_db.create_all()
+#
+#     # If any of the User, RecsClicks or Logins tables are empty in Postgres,
+#     # Create an entry for them to initilialize
+#     # This is because other functions (Dashboard, etc.) require data in these tables to be valid
+#     try:
+#         if not User.query.filter_by(user_name='harry').first():
+#             User.create_user(user='harry', email='harry@potters.com', password='secret')
+#
+#         if not RecsClicks.query.filter_by(user_id=0).first():
+#             RecsClicks.record_getrecs(user_id=0, movie_id=0, movie='None')
+#
+#         if not Logins.query.filter_by(userid=0).first():
+#             Logins.record_login(userid=0)
+#
+#         # Dash app functions require these tables below before it can be initiliazed
+#         dash_app = create_dashboard(flask_app)
+#
+#     except exc.IntegrityError:
+#
+#         flask_app.run(debug=False)
 
 ##################################################
 # OLD saving for reference
 #############################
-# flask_app = create_app('prod')
-#
-# # Create Dash Dashboard app by using the flask_app as its server
-# dash_app = create_dashboard(flask_app)
-#
-# with flask_app.app_context():
-#     # create all database tables if they don't already exist
-#     recommender_db.create_all()
-#
-#     # this code says, if user_name 'harry' does not exist in our database
-#     # create that user
-#
-#     # this is needed so that the database doesn't initialize raw with no users to compare to
-#     # which can result in database errors if 'no users exist in table' when a user tries to log in
-#     # and it is searching for users.
-#     try:
-#         if not User.query.filter_by(user_name='harry').first():
-#             User.create_user(user='harry', email='harry@potters.com', password='secret')
-#     except exc.IntegrityError:
-#         flask_app.run()
+flask_app = create_app('prod')
+
+# Create Dash Dashboard app by using the flask_app as its server
+dash_app = create_dashboard(flask_app)
+
+with flask_app.app_context():
+    # create all database tables if they don't already exist
+    recommender_db.create_all()
+
+    # this code says, if user_name 'harry' does not exist in our database
+    # create that user
+
+    # this is needed so that the database doesn't initialize raw with no users to compare to
+    # which can result in database errors if 'no users exist in table' when a user tries to log in
+    # and it is searching for users.
+    try:
+        # if not User.query.filter_by(user_name='harry').first():
+        #     User.create_user(user='harry', email='harry@potters.com', password='secret')
+
+        if not User.query.filter_by(user_name='harry').first():
+                User.create_user(user='harry', email='harry@potters.com', password='secret')
+
+            if not RecsClicks.query.filter_by(user_id=0).first():
+                RecsClicks.record_getrecs(user_id=0, movie_id=0, movie='None')
+
+            if not Logins.query.filter_by(userid=0).first():
+                Logins.record_login(userid=0)
+    except exc.IntegrityError:
+        flask_app.run()
 
 # ---------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------
@@ -97,6 +106,6 @@ with flask_app.app_context():
 #             Logins.record_login(userid=0)
 #
 #         # Dash app functions require these tables above before it can be initiliazed
-#         dash_app = create_dashboard1(flask_app)
+#         dash_app = create_dashboard(flask_app)
 #
 #         flask_app.run(debug=True)
