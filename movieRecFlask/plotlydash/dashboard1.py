f76574243378e461f85713f85c0af54fbd0ba103
thrwwy216 <thrwwy216@tutanota.com>
2020-05-14 01:11:55 -0500

    improved recommender

--- movieRecFlask/plotlydash/dashboard1.py
+++ movieRecFlask/plotlydash/dashboard1.py
@@ -30,12 +30,12 @@ def create_dashboard(server):
     ##################################################################
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/recommender_db'
     ###################################################################
-    user = 'postgres'
-    password = 'password'
-    host = 'localhost'
-    port = '5432'
-    db = 'recommender_db'
-    url = 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, db)
+    # user = 'postgres'
+    # password = 'password'
+    # host = 'localhost'
+    # port = '5432'
+    # db = 'recommender_db'
+    # url = 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, db)
 
 
     ##########################################################
@@ -43,8 +43,8 @@ def create_dashboard(server):
     ##########################################################
     # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     ##########################################################
-    # import os
-    # url = os.environ['DATABASE_URL']
+    import os
+    url = os.environ['DATABASE_URL']
 
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
